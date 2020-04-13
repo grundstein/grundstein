f6840a4a698ad07fc7b3c18697e238e3068c9fb2
jascha <jascha@jaeh.at>
2020-04-13 18:57:26 +0200

    split nodejs and user-env bootstrap scripts

---
+++ bash/bootstrap-node.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+source /grundsteinlegung/bash/config.sh
+
+printf "${YELLOW}GRUNDSTEIN${NC} starting user env generation.\n"
+
+echo "whoami? $(whoami)"
+
+printf "${YELLOW}GRUNDSTEIN${NC} installing nodejs.\n"
+
+export NVM_DIR="$USERHOME/.nvm"
+
+bash /grundsteinlegung/bash/nvm-install.sh
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+printf "${YELLOW}GRUNDSTEIN${NC} install node ${NODE_VERSION} and use it as default"
+
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
+
+printf "${GREEN}GRUNDSTEIN${NC} nodejs ${NODE_VERSION} installed.\n"
--- bash/bootstrap-user-env.sh
+++ bash/bootstrap-user-env.sh
@@ -8,34 +8,8 @@ printf "${YELLOW}GRUNDSTEIN${NC} starting user env generation.\n"
 
 echo "whoami? $(whoami)"
 
-printf "${YELLOW}GRUNDSTEIN${NC} installing nodejs.\n"
-
-export NVM_DIR="$USERHOME/.nvm"
-
-bash /grundsteinlegung/bash/nvm-install.sh
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-printf "${GRUNDSTEIN} install node ${NODE_VERSION} and use it as default"
-
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
-
-printf "${GREEN}GRUNDSTEIN${NC} nodejs installed.\n"
-
-# echo node versions
-node --version
-npm --version
-
-
-printf "${YELLOW}GRUNDSTEIN${NC} cloning grundstein services.\n"
-
-# clone the cloud env
-git clone "$GIT_URL/cloud.grundstein.it" "$USERHOME/cloud.grundstein.it"
-
-# clone the various grundstein services
+printf "${YELLOW}GRUNDSTEIN${NC} npm install grundstein services.\n"
 
 npm install -g grundstein/gms grundstein/gas
 
-printf "${GREEN}GRUNDSTEIN${NC} user env generation finished successfully.\n"
+printf "${GREEN}GRUNDSTEIN${NC} npm install finished successfully.\n"

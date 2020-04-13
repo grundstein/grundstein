b64b067256f6c1662ff3a399b277bd8d7da12160
jascha <jascha@jaeh.at>
2020-04-13 20:19:51 +0200

    clone cloud.grundstein.it and set it up

--- bash/bootstrap-services.sh
+++ bash/bootstrap-services.sh
@@ -4,12 +4,16 @@ set -euf -o pipefail
 
 source /grundsteinlegung/bash/config.sh
 
-printf "${YELLOW}GRUNDSTEIN${NC} starting user env generation.\n"
+printf "${YELLOW}GRUNDSTEIN${NC} starting grundstein service setup.\n"
 
 echo "whoami? $(whoami)"
+
+printf "${YELLOW}GRUNDSTEIN${NC} git clone cloud.grundstein.it\n"
+
+git clone "git://github.com/grundstein/${GIT_CLOUD_REPO}" "/home/${USERNAME}/${GIT_CLOUD_REPO}"
 
 printf "${YELLOW}GRUNDSTEIN${NC} npm install grundstein services.\n"
 
 npm install -g grundstein/gms grundstein/gas grundstein/gps
 
-printf "${GREEN}GRUNDSTEIN${NC} npm install finished successfully.\n"
+printf "${GREEN}GRUNDSTEIN${NC} service setup finished successfully.\n"

533e5c02d12f0314afece2e9d189979213654253
jascha <jascha@jaeh.at>
2020-04-13 16:12:17 +0200

    bootstrap: also create user, rewrite install, fix TZ

--- bash/bootstrap-grundstein.sh
+++ bash/bootstrap-grundstein.sh
@@ -2,39 +2,54 @@
 
 set -euf -o pipefail
 
-export TZ=Europe/Vienna
+export TZ="Europe/Vienna"
+
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+export USERNAME="grundstein"
+export USERHOME="/home/grundstein"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-printf "${GREEN}GRUNDSTEIN${NC} starting bootstrap\n"
+printf "${YELLOW}GRUNDSTEIN${NC} starting bootstrap\n"
 
 # update apt sources
-apt update -q -y
+apt -y update
 
 # install git
-apt install -q -y git software-properties-common makepasswd curl nano python3-pip
+apt -y install git software-properties-common makepasswd curl nano python3-pip
 
 # prepare certbot install
-add-apt-repository -q universe
-add-apt-repository -q ppa:certbot/certbot
-apt update
+add-apt-repository -y universe
+add-apt-repository -y ppa:certbot/certbot
+apt -y update
 
 # actually install certbot
-apt install -y -q certbot
+TZ=Europe/Vienna && apt -y install certbot
 
 # install certbot digitalocean plugin.
-pip3 install -q certbot-dns-digitalocean
+pip3 install certbot-dns-digitalocean
 
 # update packages
-apt -y -q upgrade
+apt -y upgrade
 
 # cleanup unneeded packages
-apt -y -q autoremove
+apt -y autoremove
 
-printf "${GREEN}GRUNDSTEIN${NC} apt installation done."
+printf "${GREEN}GRUNDSTEIN${NC} apt installation done.\n"
 
-print "${GREEN}GRUNDSTEIN${NC}"
+printf "${YELLOW}GRUNDSTEIN${NC} starting user generation\n"
+# remove user if it exists
+
+id -u "$USERNAME" &>/dev/null && userdel grundstein
+
+# add user. one should be fine for now. we do not need to know the password
+PASSWORD=$(makepasswd --min 42 --max 42)
+useradd -m -p "$PASSWORD" -d "$USERHOME" -s /bin/bash "$USERNAME"
+
+printf "${GREEN}GRUNDSTEIN${NC} user generated successfully.\n"
 
 printf "${GREEN}GRUNDSTEIN${NC} bootstrap done\n"

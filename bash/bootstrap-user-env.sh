7e5d53e7febd1d3f00a9866138d679f1f764467e
jascha <jascha@jaeh.at>
2020-04-13 16:58:19 +0200

    bash/config: add colors, bootstrap-user-env: add NVM_DIR setup

--- bash/bootstrap-user-env.sh
+++ bash/bootstrap-user-env.sh
@@ -16,6 +16,9 @@ export NVM_DIR="$USERHOME/.nvm" && (
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 # install node 13 and use it as default
 nvm install $NODE_VERSION
 nvm use $NODE_VERSION
--- bash/config.sh
+++ bash/config.sh
@@ -4,3 +4,8 @@ export USERHOME="/home/grundstein"
 export NODE_VERSION=13
 export NVM_DIR="$USERHOME/.nvm"
 export GIT_URL="git://github.com/grundstein"
+
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[1;33m'
+export NC='\033[0m' # No Color

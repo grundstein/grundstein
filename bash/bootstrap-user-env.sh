e7062bf09c3f0d8f2fe933ba088a1574709b2744
jascha <jascha@jaeh.at>
2020-04-13 18:03:28 +0200

    bootstrap-user-env: use nvm directly

--- bash/bootstrap-user-env.sh
+++ bash/bootstrap-user-env.sh
@@ -16,12 +16,13 @@ git clone https://github.com/nvm-sh/nvm.git $NVM_DIR
 cd $NVM_DIR
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 
+nvm --version
+
 # install node 13 and use it as default
-$NVM_DIR/nvm.sh install $NODE_VERSION
-$NVM_DIR/nvm.sh use $NODE_VERSION
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
 
 # echo node versions
-$NVM_DIR/nvm.sh --version
 node --version
 npm --version
 

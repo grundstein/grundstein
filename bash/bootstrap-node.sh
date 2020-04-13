ca2a02159913d22b444f14e6c8d7c572c9e897c3
jascha <jascha@jaeh.at>
2020-04-13 19:22:39 +0200

    rename NODE_VERSION to NVM_NODE_VERSION

--- bash/bootstrap-node.sh
+++ bash/bootstrap-node.sh
@@ -15,11 +15,11 @@ bash /grundsteinlegung/bash/nvm-install.sh
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-printf "${YELLOW}GRUNDSTEIN${NC} install node ${NODE_VERSION} and use it as default"
+printf "${YELLOW}GRUNDSTEIN${NC} install node ${NVM_NODE_VERSION} and use it as default"
 
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
+nvm install $NVM_NODE_VERSION
+nvm use $NVM_NODE_VERSION
 
 node --version
 
-printf "${GREEN}GRUNDSTEIN${NC} nodejs ${NODE_VERSION} installed.\n"
+printf "${GREEN}GRUNDSTEIN${NC} nodejs ${NVM_NODE_VERSION} installed.\n"
--- bash/config.sh
+++ bash/config.sh
@@ -1,7 +1,7 @@
 export TZ="Europe/Vienna"
 export USERNAME="grundstein"
 export USERHOME="/home/grundstein"
-export NODE_VERSION="13"
+export NVM_NODE_VERSION=13
 export NVM_DIR="$USERHOME/.nvm"
 export GIT_URL="git://github.com/grundstein"
 

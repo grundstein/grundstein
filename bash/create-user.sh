a55ecd7b088a0ace385b7dbacd08b9128da5f2fa
jascha <jascha@jaeh.at>
2020-04-15 10:16:41 +0200

    bash/create-user.sh: only add user if it does not exist

--- bash/create-user.sh
+++ bash/create-user.sh
@@ -7,11 +7,12 @@ export NC='\033[0m' # No Color
 
 printf "\${YELLOW}GRUNDSTEIN\${NC} starting user generation\\n"
 
-# remove user if it exists
-id -u "$USERNAME" &>/dev/null && userdel grundstein
-
-# add user. one should be fine for now. we do not need to know the password
-PASSWORD=$(makepasswd --min 42 --max 42)
-useradd -m -p "$PASSWORD" -d "$USERHOME" -s /bin/bash "$USERNAME"
+# add user if it does not exist.
+# one should be fine for now.
+# we do not need to know the password.
+id -u "$USERNAME" &>/dev/null || (
+  PASSWORD=$(makepasswd --min 42 --max 42)
+  useradd -m -p "$PASSWORD" -d "$USERHOME" -s /bin/bash "$USERNAME"
+)
 
 printf "\${GREEN}GRUNDSTEIN\${NC} user generated successfully.\\n"

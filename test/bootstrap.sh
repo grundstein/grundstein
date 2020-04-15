b9bcc06de40191625f2410bf09e207f8ba31f46f
jascha <jascha@jaeh.at>
2020-04-15 10:26:04 +0200

    update test/bootstrap.sh

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -2,8 +2,15 @@
 
 set -euf -o pipefail
 
-CONTAINER_NAME="grundstein-install-test"
-INSTALL_FILE="bootstrap-grundstein.sh"
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[1;33m'
+export NC='\033[0m' # No Color
+
+CONTAINER_NAME='grundstein-install-test'
+INSTALL_FILE='grundsteinlegung.sh'
+
+printf "${YELLOW}GRUNDSTEIN${NC} - this script runs docker, it needs the root.\n"
 
 sudo docker build test --tag="$CONTAINER_NAME"
 
@@ -13,8 +20,6 @@ fi
 
 sudo docker run --rm -td --name="$CONTAINER_NAME" "$CONTAINER_NAME"
 
-sudo docker cp "./bash/$INSTALL_FILE" "$CONTAINER_NAME:/"
-sudo docker cp "./bash/config.sh" "$CONTAINER_NAME:/"
+sudo docker cp "./bootstrap/$INSTALL_FILE" "$CONTAINER_NAME:/"
 
 sudo docker exec -it "$CONTAINER_NAME" /bin/bash /$INSTALL_FILE
-

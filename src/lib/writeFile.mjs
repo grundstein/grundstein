443b678e2520a8d5b5092cbe4db99d258d496679
jascha <jascha@jaeh.at>
2020-04-16 18:26:50 +0200

    lib/writeFile: rename sh file imports

--- src/lib/writeFile.mjs
+++ src/lib/writeFile.mjs
@@ -2,13 +2,13 @@ import path from 'path'
 import { log } from '@grundstein/commons'
 import { fs } from '@magic/fs'
 
-import bash from '../bash/index.mjs'
+import sh from '../sh/index.mjs'
 
 export const writeFile = async ({ dir, name, config }) => {
   const file = path.join(dir, `${name}.sh`)
 
   try {
-    const contents = bash[name](config)
+    const contents = sh[name](config)
 
     await fs.writeFile(file, contents)
   } catch (e) {

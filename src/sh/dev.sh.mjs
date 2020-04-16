34151ee71f6b2bac1ec07533a22e1d3f094e1f54
jascha <jascha@jaeh.at>
2020-04-16 21:11:07 +0200

    sh/dev.sh.mjs: add

---
+++ src/sh/dev.sh.mjs
@@ -0,0 +1,42 @@
+import { log } from '@grundstein/commons'
+
+import { writeFile } from '../lib/index.mjs'
+
+export default async config => {
+  const { dir } = config
+
+  const containerName = 'grundstein-dev'
+  const installFile = 'grundsteinlegung.sh'
+
+  const contents = `
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+printf "GRUNDSTEIN - running a development build.\\n\\n\\n"
+
+printf "GRUNDSTEIN - this script runs docker, it needs the root.\\n\\n\\n"
+
+if [ ! -f bootstrap/dev_rsa ]; then
+  ssh-keygen -f bootstrap/dev_rsa -t rsa -N ''
+fi
+
+sudo docker build dev --tag="${containerName}"
+
+if [[ $(sudo docker ps -q -f name=${containerName}) ]]; then
+   sudo docker rm "${containerName}" -f
+fi
+
+sudo docker run --rm -td --name="${containerName}" "${containerName}"
+
+sudo docker cp "./bootstrap/${installFile}" "${containerName}:/"
+
+sudo docker cp "./bootstrap/dev_rsa.pub" "${containerName}:/.ssh"
+
+sudo docker exec -it "${containerName}" /usr/bin/env bash /${installFile}
+`.trimStart()
+
+  await writeFile({ name: 'dev', config, contents, dir })
+
+  return contents
+}

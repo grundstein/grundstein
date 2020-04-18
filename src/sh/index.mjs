7d5004af0589c4a09e4d1adedafbfb1c4fbafd3d
jascha <jascha@jaeh.at>
2020-04-18 10:45:42 +0200

    sh/index.mjs: export docker shell script

--- src/sh/index.mjs
+++ src/sh/index.mjs
@@ -1,9 +1,11 @@
 import dev from './dev.sh.mjs'
+import docker from './docker.sh.mjs'
 import grundsteinlegung from './grundsteinlegung.sh.mjs'
 import services from './services.sh.mjs'
 
 export default {
   dev,
+  docker,
   grundsteinlegung,
   services,
 }

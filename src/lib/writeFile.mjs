dcc4410f3f8c7bac472879dc2cb261eac2d7f828
jascha <jascha@jaeh.at>
2020-04-18 11:16:56 +0200

    writeFile: mkdirp the directory the file lives in

--- src/lib/writeFile.mjs
+++ src/lib/writeFile.mjs
@@ -7,7 +7,11 @@ import sh from '../sh/index.mjs'
 export const writeFile = async ({ dir, name, config, contents }) => {
   const file = path.join(dir, `${name}.sh`)
 
+  const fileDir = path.dirname(file)
+
   try {
+    await fs.mkdirp(fileDir)
+
     await fs.writeFile(file, contents)
   } catch (e) {
     log.error('E_WRITE_FILE', e)

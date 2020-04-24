37bc631c5e5ed628a871c341b3f05c7358b154cf
jascha <jascha@jaeh.at>
2020-04-24 14:15:31 +0200

    sh/grundsteinlegung: nicer prints

--- src/sh/grundsteinlegung.sh.mjs
+++ src/sh/grundsteinlegung.sh.mjs
@@ -24,23 +24,23 @@ export DEBIAN_FRONTEND=noninteractive
 
 
 
-printf "${YELLOW}log dir${NC} - mkdir ${logDir}\\n\\n"
+printf "${YELLOW}log dir${NC} - mkdir ${logDir}"
 
 mkdir -p ${logDir}
 
-printf "${logDir} ${GREEN}created${NC}\\n\\n"
+printf " - ${GREEN}created${NC}\\n\\n"
 
 
 
-printf "${YELLOW}apt update${NC}\\n"
+printf "${YELLOW}apt update${NC}"
 
 apt-get -y update >> ${INSTALL_LOG} 2>> ${ERROR_LOG}
 
-printf "apt update: ${GREEN}done${NC}\\n\\n"
+printf " - ${GREEN}done${NC}\\n\\n"
 
 
 
-printf "${YELLOW}timezones${NC} - setup\\n"
+printf "${YELLOW}timezones${NC} - setup"
 
 ln -fs /usr/share/zoneinfo/${env.TZ} /etc/localtime
 
@@ -48,11 +48,11 @@ apt-get install -y tzdata >> ${INSTALL_LOG} 2>> ${ERROR_LOG}
 
 dpkg-reconfigure -f noninteractive tzdata >> ${INSTALL_LOG} 2>> ${ERROR_LOG}
 
-printf "timezones: ${GREEN}done${NC}\\n\\n"
+printf " - ${GREEN}done${NC}\\n\\n"
 
 
 
-printf "${YELLOW}install${NC} dependencies\\n"
+printf "${YELLOW}install${NC} dependencies"
 
 # curl needed for nvm
 # makepasswd needed for user generation below
@@ -67,35 +67,35 @@ ntp \
 nano \
 >> ${INSTALL_LOG} 2>> ${ERROR_LOG}
 
-printf "install dependencies: ${GREEN}done${NC}\\n\\n"
+printf " - ${GREEN}done${NC}\\n\\n"
 
 
 
-printf "${YELLOW}apt upgrade${NC}\\n"
+printf "${YELLOW}apt upgrade${NC}"
 
 apt-get -qq -y upgrade >> ${INSTALL_LOG} 2>> ${ERROR_LOG}
 
-printf "apt upgrade: ${GREEN}done${NC}\\n\\n"
+printf " - ${GREEN}done${NC}\\n\\n"
 
 
 
-printf "${YELLOW}apt autoremove${NC}\\n"
+printf "${YELLOW}apt autoremove${NC}"
 
 apt-get -y autoremove >> ${INSTALL_LOG} 2>> ${ERROR_LOG}
 
-printf "apt autoremove ${GREEN}done${NC}\\n\\n"
+printf " - ${GREEN}done${NC}\\n\\n"
 
 
 
-printf "${YELLOW}git clone${NC} grundsteinlegung.\\n"
+printf "${YELLOW}git clone${NC} grundsteinlegung"
 
 git clone  "${env.GIT_URL}/cli" /grundsteinlegung >> ${INSTALL_LOG} 2>> ${ERROR_LOG}
 
-printf "grundsteinlegung ${GREEN}cloned${NC}\\n\\n"
+printf " - ${GREEN}cloned${NC}\\n\\n"
 
 
 
-printf "${YELLOW}user generation${NC}\\n"
+printf "${YELLOW}user generation${NC}"
 
 # add user if it does not exist.
 # one should be fine for now.
@@ -105,14 +105,14 @@ id -u "${env.USERNAME}" &>/dev/null || (
   useradd -m -p "$PASSWORD" -d "${env.USERHOME}" -s /bin/bash "${env.USERNAME}"
 )
 
-printf "user generation: ${GREEN}done${NC}\\n\\n"
+printf " - ${GREEN}done${NC}\\n\\n"
 
 
-printf "${YELLOW}install${NC} nodejs\\n"
-# install nodejs
+printf "${YELLOW}install${NC} nodejs"
+
 /usr/bin/env bash /grundsteinlegung/bash/node.sh >> ${INSTALL_LOG} 2>> ${ERROR_LOG}
 
-printf "nodejs install ${GREEN}done${NC}\\n\\n"
+printf " - ${GREEN}done${NC}\\n\\n"
 
 `.trimStart()
 

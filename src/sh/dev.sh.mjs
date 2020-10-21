6f0b91570469cd3c06eac4c16cd0d12f9e4b8cfd
jascha <jascha@jaeh.at>
2020-10-21 18:02:02 +0200

    dev.sh.mjs: remove variable and embed in string, update stdout

--- src/sh/dev.sh.mjs
+++ src/sh/dev.sh.mjs
@@ -1,6 +1,6 @@
 import path from 'path'
 
-import { fs, log } from '@grundstein/commons'
+import { fs } from '@grundstein/commons'
 
 import { colors, writeFile } from '../lib/index.mjs'
 
@@ -26,9 +26,45 @@ export default async config => {
     devDirectory = path.join(dir, '..', 'node_modules', 'grundstein', 'dev')
   }
 
-  const hostInitScripts = hostScripts
-    .map(script =>
-      `
+  const contents = `
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+printf "${YELLOW}GRUNDSTEIN DEV DOCKER${NC} \\n\\n"
+
+printf "this script builds and runs docker, it needs the root.\\n\\n\\n"
+
+# remove docker if it is running
+if [[ $(sudo docker ps -aq -f name=${containerName}) ]]; then
+  sudo docker rm -f "${containerName}"
+fi
+
+sudo docker build ${devDirectory} --tag="${containerName}"
+
+# TODO: change 2323:4343 to 443:4343 once the local certificates are being generated.
+sudo docker run \
+--cap-add=NET_ADMIN \
+--privileged=true \
+-td \
+-p 2350:2350 \
+-p 2351:2351 \
+-p 80:8080 \
+-p 2323:4343 \
+--name="${containerName}" \
+"${containerName}"
+
+sudo docker cp "./bootstrap/${installFile}" "${containerName}:/"
+
+sudo docker exec -it "${containerName}" /usr/bin/env bash /${installFile}
+
+printf "${YELLOW}docker${NC} - dev build - ${GREEN}done${NC}\\n\\n\\n"
+
+
+
+${hostScripts
+      .map(script =>
+        `
 printf "${YELLOW}add init script${NC}: ${script}\\n"
 
 sudo docker exec -it ${containerName} mkdir -p "${hostDir}"
@@ -41,36 +77,8 @@ sudo docker exec -it ${containerName} /usr/bin/env bash ${hostDir}/${path.basena
 
 printf "add init script: ${GREEN}done${NC}\\n\\n"
 `.trim(),
-    )
-    .join('\n\n')
-
-  const contents = `
-#!/usr/bin/env bash
-
-set -euf -o pipefail
-
-printf "${YELLOW}GRUNDSTEIN DEV DOCKER${NC} \\n\\n"
-
-printf "this script builds and runs docker, it needs the root.\\n\\n\\n"
-
-if [[ $(sudo docker ps -aq -f name=${containerName}) ]]; then
-  sudo docker rm -f "${containerName}"
-fi
-
-sudo docker build ${devDirectory} --tag="${containerName}"
-
-# TODO: change 2323:4343 to 443:4343 once the local certificates are being generated.
-sudo docker run -td -p 2350:2350 -p 2351:2351 -p 80:8080 -p 2323:4343 --name="${containerName}" "${containerName}"
-
-sudo docker cp "./bootstrap/${installFile}" "${containerName}:/"
-
-sudo docker exec -it "${containerName}" /usr/bin/env bash /${installFile}
-
-printf "docker dev build: ${GREEN}done${NC}\\n\\n\\n"
-
-
-
-${hostInitScripts}
+      )
+      .join('\n\n')}
 
 
 
